ee17f4da1aef72c9a918b50db1957f01a0f4502c
ankumar12 <96569601+ankumar12@users.noreply.github.com>
2021-12-23 14:55:21 +0530

    Delete twilio-sfmc-master directory

--- twilio-sfmc-master/lib/jwtDecoder.js
+++
@@ -1,7 +0,0 @@
-const JWT = require('jsonwebtoken');
-
-module.exports = (body, secret, cb) => {
-	require('jsonwebtoken').verify(body.toString('utf8'), secret, {
-		algorithm: 'HS256'
-	}, cb);
-};
